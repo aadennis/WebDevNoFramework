85f84d4c31ada7c72103e731ce6ca7baa1a78252
aadennis <dennis.circ@gmail.com>
2025-09-12 17:41:08 +0100

    fingering position fixed

--- Svg/Chords/code/piano/fingeringMap.js
+++ Svg/Chords/code/piano/fingeringMap.js
@@ -1,36 +1,39 @@
 const whiteKeyX = {
   'F-2': 0,
-  'G-2': 60,
-  'A-2': 120,
-  'B-2': 180,
-  'C-3': 240,
-  'D-3': 300,
-  'E-3': 360,
-  'F-3': 420,
-  'G-3': 480,
-  'A-3': 540,
-  'B-3': 600,
-  'C-4': 660
+  'G-2': 42,
+  'A-2': 84,
+  'B-2': 126,
+  'C-3': 168,
+  'D-3': 210,
+  'E-3': 252,
+  'F-3': 294,
+  'G-3': 336,
+  'A-3': 378,
+  'B-3': 420,
+  'C-4': 462,
 };
 
 const blackKeyX = {
-  'F#-2': 40,
-  'G#-2': 100,
-  'A#-2': 160,
-  'C#-3': 280,
-  'D#-3': 340,
-  'F#-3': 460,
-  'G#-3': 520,
-  'A#-3': 580,
-  'C#-4': 700
+  'F#-2': 28,
+  'G#-2': 70,
+  'A#-2': 112,
+  'C#-3': 196,
+  'D#-3': 238,
+  'F#-3': 322,
+  'G#-3': 364,
+  'A#-3': 406,
+  'C#-4': 490,
 };
+
+const WHITE_KEY_WIDTH = 42;
+const BLACK_KEY_WIDTH = 28;
 
 function getFingeringPosition(note) {
   if (whiteKeyX[note] !== undefined) {
-    return { x: whiteKeyX[note] + 30, anchor: '#fingering-anchor' };
+    return { x: whiteKeyX[note] + WHITE_KEY_WIDTH / 2, anchor: '#fingering-anchor' };
   }
   if (blackKeyX[note] !== undefined) {
-    return { x: blackKeyX[note] + 20, anchor: '#fingering-anchor-black' };
+    return { x: blackKeyX[note] + BLACK_KEY_WIDTH / 2, anchor: '#fingering-anchor-black' };
   }
   return null;
 }
--- Svg/Chords/code/piano/scale_out_keys.py
+++ Svg/Chords/code/piano/scale_out_keys.py
@@ -20,3 +20,26 @@ black_key_offsets = [1, 2, 3, 5, 6, 8, 9, 10, 12]  # Indexes between white keys
 for i in black_key_offsets:
     center = i * white_spacing - (black_key_width / 2)
     print(f'<rect x="{round(center, 1)}" y="0" width="{black_key_width}" height="120" fill="black" />')    
+
+print("-----------------------------")
+white_keys = ['F-2', 'G-2', 'A-2', 'B-2', 'C-3', 'D-3', 'E-3', 'F-3', 'G-3', 'A-3', 'B-3', 'C-4']
+scaled_spacing = 42
+
+for i, key in enumerate(white_keys):
+    x = i * scaled_spacing
+    print(f"  '{key}': {x},")
+
+print("-----------------------------")
+
+black_keys = {
+    'F#-2': 1, 'G#-2': 2, 'A#-2': 3,
+    'C#-3': 5, 'D#-3': 6,
+    'F#-3': 8, 'G#-3': 9, 'A#-3': 10,
+    'C#-4': 12
+}
+
+for key, index in black_keys.items():
+    center = index * scaled_spacing - 14  # 14 = half of 28px width
+    print(f"  '{key}': {round(center)},")
+
+    
